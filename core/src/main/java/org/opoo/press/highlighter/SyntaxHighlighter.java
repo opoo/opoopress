055998e49e46aa14546f60683fe8620d2d112a3e
Alex Lin <alex@opoo.org>
2013-06-22 02:09:19 +0800

    Moved highlighter related classes to new package.

--- core/src/main/java/org/opoo/press/highlighter/SyntaxHighlighter.java
+++ core/src/main/java/org/opoo/press/highlighter/SyntaxHighlighter.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.opoo.press.converter;
+package org.opoo.press.highlighter;
 
 import java.util.List;
 
