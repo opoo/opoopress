f59a1f527b839a4da209a1fa7ca6c1134c3b94a2
Alex Lin <alex@opoo.org>
2013-07-05 20:54:31 +0800

    Added sass

---
+++ core/src/main/resources/site/config.rb
@@ -0,0 +1,16 @@
+
+# see http://compass-style.org/help/tutorials/configuration-reference/
+
+sass_dir = "sass"
+css_dir = "assets/stylesheets"
+images_dir = "assets/images"
+javascripts_dir = "assets/javascripts"
+cache_dir = "../public/sass-cache"
+
+http_images_path = "/images"
+http_stylesheets_path = "/stylesheets"
+http_javascripts_path = "/javascripts"
+
+# nested, :expanded, :compact, or :compressed.
+output_style = :compressed
+line_comments = false
