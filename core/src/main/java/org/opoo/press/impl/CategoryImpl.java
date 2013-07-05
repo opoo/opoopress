5dcbd0a99278783d16bde3ddcb0d9771232db4e3
Alex Lin <alex@opoo.org>
2013-07-05 08:20:56 +0800

    Add Tag and Category classes.

---
+++ core/src/main/java/org/opoo/press/impl/CategoryImpl.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2013 Alex Lin.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.opoo.press.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opoo.press.Category;
+import org.opoo.press.Post;
+import org.opoo.press.Site;
+import org.opoo.press.util.MapUtils;
+import org.opoo.press.util.Utils;
+
+/**
+ * @author Alex Lin
+ *
+ */
+public class CategoryImpl implements Category{
+	private Category parent;
+	private List<Category> children = new ArrayList<Category>();
+	private String nicename;
+	private String name;
+	private List<Post> posts = new ArrayList<Post>();
+	private Site site;
+	
+	private String path;
+	private String title;
+	private String url;
+
+	public CategoryImpl(String nicename, String name, Site site){
+		this(nicename, name, null, site);
+	}
+	
+	public CategoryImpl(String nicename, String name, Category parent, Site site) {
+		this.parent = parent;
+		this.nicename = nicename;
+		this.name = name;
+		this.site = site;
+		init();
+	}
+
+	private void init(){
+		if(parent != null){
+			parent.getChildren().add(this);
+		}
+		boolean categoryTree = MapUtils.get(site.getConfig(), "category_tree", false);
+		
+		if(categoryTree && parent != null){
+			path =  parent.getPath() + "." + nicename;
+		}else{
+			path = nicename;
+		}
+		
+		if(categoryTree && parent != null){
+			url = parent.getUrl() + Utils.encodeURL(nicename) + "/";
+		}else{
+			String categoryDir = (String)MapUtils.get(site.getConfig(), "category_dir", "");
+			url = categoryDir + "/" + Utils.encodeURL(nicename) + "/";
+		}
+		
+		if(categoryTree && parent != null){
+			title = parent.getTitle() + " &#8250; " + name;
+		}else{
+			title = name;
+		}
+	}
+	
+	/**
+	 * @return the parent
+	 */
+	public Category getParent() {
+		return parent;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.opoo.press.Category#getChildren()
+	 */
+	@Override
+	public List<Category> getChildren() {
+		return children;
+	}
+
+	/**
+	 * @return the nicename
+	 */
+	public String getNicename() {
+		return nicename;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the posts
+	 */
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public boolean isNameOrNicename(String nameOrNicename){
+		if(nameOrNicename.equals(getNicename())){
+			return true;
+		}
+		
+		if(nameOrNicename.equals(getName())){
+			return true;
+		}
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.opoo.press.Category#getTitle()
+	 */
+	@Override
+	public String getTitle() {
+		return title;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.opoo.press.Category#getUrl()
+	 */
+	@Override
+	public String getUrl() {
+		//String categoryDir = (String) config.get("category_dir");
+		//return /*rootUrl + */ categoryDir + "/" + category.getUrl() + "/";
+		return url;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.opoo.press.Category#getPath()
+	 */
+	@Override
+	public String getPath() {
+		return path;
+	}
+
+}
---
+++ core/src/main/java/org/opoo/press/impl/TagImpl.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2013 Alex Lin.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.opoo.press.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opoo.press.Post;
+import org.opoo.press.Site;
+import org.opoo.press.Tag;
+import org.opoo.press.util.MapUtils;
+import org.opoo.press.util.Utils;
+
+/**
+ * @author Alex Lin
+ *
+ */
+public class TagImpl implements Tag {
+	private String slug;
+	private String name;
+	private List<Post> posts = new ArrayList<Post>();
+	
+	private String url;
+	/**
+	 * @param slug
+	 * @param name
+	 */
+	public TagImpl(String slug, String name, Site site) {
+		this.slug = slug;
+		this.name = name;
+		
+		String tagDir = (String) MapUtils.get(site.getConfig(), "tag_dir", "");
+		this.url = /*rootUrl + */ tagDir + "/" + Utils.encodeURL(slug) + "/";
+	}
+	/**
+	 * @return the slug
+	 */
+	public String getSlug() {
+		return slug;
+	}
+
+	/**
+	 * @param slug the slug to set
+	 */
+	public void setSlug(String slug) {
+		this.slug = slug;
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/* (non-Javadoc)
+	 * @see org.opoo.press.Tag#isNameOrSlug(java.lang.String)
+	 */
+	@Override
+	public boolean isNameOrSlug(String nameOrSlug) {
+		if(nameOrSlug.equals(getSlug())){
+			return true;
+		}
+		
+		if(nameOrSlug.equals(getName())){
+			return true;
+		}
+		return false;
+	}
+	/* (non-Javadoc)
+	 * @see org.opoo.press.Tag#getUrl()
+	 */
+	@Override
+	public String getUrl() {
+		return url;
+	}
+	/* (non-Javadoc)
+	 * @see org.opoo.press.PostsHolder#getPosts()
+	 */
+	@Override
+	public List<Post> getPosts() {
+		return posts;
+	}
+}
