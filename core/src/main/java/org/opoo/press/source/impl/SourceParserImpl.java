a45e2ad87d229edecbe82dccc0223ef05f633d23
Alex Lin <alex@opoo.org>
2013-07-16 18:00:08 +0800

    Optimize imports.

--- core/src/main/java/org/opoo/press/source/impl/SourceParserImpl.java
+++ core/src/main/java/org/opoo/press/source/impl/SourceParserImpl.java
@@ -25,7 +25,6 @@ import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.LineIterator;
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.opoo.press.source.NoFrontMatterException;
